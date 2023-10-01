49a7a4f024f4265168f43a15a0c2a5d6623a6bf3
RaptorCoding <raptorlearningcode@gmail.com>
2023-10-02 00:28:52 +0200

    Finished GUI Calculator next project ETA: ~1 Week

--- gui_process/gui.py
+++ gui_process/gui.py
@@ -1,13 +1,39 @@
 from tkinter import *
 
-def button_press():
-    pass
+def button_press(num):
+
+    global equation_text
+
+    equation_text = equation_text + str(num)
+    equation_label.set(equation_text)
 
 def equals():
-    pass
+
+    global equation_text
+
+    try:
+
+        total = str(eval(equation_text))
+
+        equation_label.set(total)
+
+        equation_text = total
+
+    except ZeroDivisionError:
+
+        equation_label.set("0")
+
+    except SyntaxError:
+        equation_label.set("Error:Syntax invalid")
+
 
 def clear():
-    pass
+    
+    global equation_text
+
+    equation_label.set("")
+
+    equation_text = ""
 
 window = Tk()
 
