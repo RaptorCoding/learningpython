5b2265e42564fe36497e18320397f19db791f07e
RaptorCoding <raptorlearningcode@gmail.com>
2023-10-01 23:58:19 +0200

    W.I.P file still placing buttons

--- gui_process/gui.py
+++ gui_process/gui.py
@@ -9,15 +9,60 @@ def equals():
 def clear():
     pass
 
-root = Tk()
-root.geometry("300x400")
-root.title("Calculator")
+window = Tk()
 
-button = Button(root, height=3,)
+window.title("Testing")
+window.geometry("500x500")
 
-equationtext = ""
+equation_text = ""
+
 equation_label = StringVar()
 
-label = Label(root, textvariable=equation_label, font=('Comicsans'))
+label = Label(window, textvariable=equation_label, font=('comicsans',20), bg="white", width=24, height=2)
+label.pack()
 
-root.mainloop()
+frame = Frame(window)
+frame.pack()
+
+button1 = Button(frame, text=1, height=4, width=9, font=35,
+                 command=lambda: button_press(1))
+button1.grid(row=0, column=0)
+
+button2 = Button(frame, text=2, height=4, width=9, font=35,
+                 command=lambda: button_press(2))
+button2.grid(row=0, column=1)
+
+button3 = Button(frame, text=3, height=4, width=9, font=35,
+                 command=lambda: button_press(3))
+button3.grid(row=0, column=2)
+
+button4 = Button(frame, text=4, height=4, width=9, font=35,
+                 command=lambda: button_press(4))
+button4.grid(row=1, column=0)
+
+button5 = Button(frame, text=5, height=4, width=9, font=35,
+                 command=lambda: button_press(5))
+button5.grid(row=1, column=1)
+
+button6 = Button(frame, text=6, height=4, width=9, font=35,
+                 command=lambda: button_press(6))
+button6.grid(row=1, column=2)
+
+button7 = Button(frame, text=7, height=4, width=9, font=35,
+                 command=lambda: button_press(7))
+button7.grid(row=2, column=0)
+
+button8 = Button(frame, text=8, height=4, width=9, font=35,
+                 command=lambda: button_press(8))
+button8.grid(row=2, column=1)
+
+button9 = Button(frame, text=9, height=4, width=9, font=35,
+                 command=lambda: button_press(9))
+button9.grid(row=2, column=2)
+
+button0 = Button(frame, text=0, height=4, width=9, font=35,
+                 command=lambda: button_press(0))
+button0.grid(row=3, column=1)
+
+
+window.mainloop()
